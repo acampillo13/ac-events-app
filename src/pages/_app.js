e6eb3d286ce4befe10ea3016a8dca266f0e43421
AC Code <accode@Mac-mini-de-AC.local>
2024-11-23 02:03:16 +0100

    Estructura del sass

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap-grid.min.css'; // Importa solo el CSS del grid
+import '../styles/globals.scss';  // Si tienes estilos globales personalizados en SCSS
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
